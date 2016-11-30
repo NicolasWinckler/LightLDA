d6ac612dea965bf3dd77c66c0eac8ada1986fe57
Nicolas Winckler <nw@quanox.com>
2016-11-30 15:47:49 +0100

    generate static library. move the third party directory hints to the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
-# build lightlda
+#!/bin/bash
 
+# build lightlda
 #git clone -b multiverso-initial git@github.com:Microsoft/multiverso.git
 git clone -b multiverso-initial git@github.com:NicolasWinckler/multiverso.git
+MULTIVERSO_DIR="$PWD/multiverso"
+THIRDPARTY_DIR="$PWD/multiverso/third_party"
+CMAKE_ARGS="-DUSE_MULTIVERSO_PATH=$MULTIVERSO_DIR"
+CMAKE_ARGS+=" -DUSE_ZMQ_PATH=$THIRDPARTY_DIR"
+CMAKE_ARGS+=" -DMPI_CXX_COMPILER=$THIRDPARTY_DIR/bin/mpicxx"
+
+# install lightlda dependencies, i.e. multiverso, zmq and mpi
 cd multiverso
 cd third_party
 sh install.sh
@@ -9,8 +17,11 @@ cd ..
 make -j4 all
 
 cd ..
+# install lightlda
 mkdir build
 cd build
-cmake ..
+cmake $CMAKE_ARGS ..
 make -j4
 cd ..
+
+
